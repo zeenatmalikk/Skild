5f776ecd39ca3c8608920bdaa9944a8044562475
Zeenat K Malik <zeenatkm301@gmail.com>
2026-05-14 17:09:12 +0530

    first commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,87 @@
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import type { QueryClient } from "@tanstack/react-query";
+import {
+	createRootRouteWithContext,
+	HeadContent,
+	Scripts,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import Crosshair from "#/components/Crosshair";
+import Navbar from "#/components/Navbar";
+import ClerkProvider from "../integrations/clerk/provider";
+import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
+import appCss from "../styles.css?url";
+
+interface MyRouterContext {
+	queryClient: QueryClient;
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "Skild - The registry for Agentic Intelligence",
+			},
+			{
+				name: "description",
+				content:
+					"Skild is the registry for Agentic Intelligence, a collection of agents and tools that can be used to build intelligent applications.",
+			},
+		],
+		links: [
+			{
+				rel: "stylesheet",
+				href: appCss,
+			},
+		],
+	}),
+	shellComponent: RootDocument,
+});
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<head>
+				<HeadContent />
+			</head>
+			<body className="font-sans antialiased wrap-anywhere">
+				<ClerkProvider>
+					<div id="root-layout">
+						<header>
+							<div className="frame">
+								<Navbar />
+								<Crosshair/>
+								<Crosshair/>
+							</div>
+						</header>
+            <main>
+              <div className="frame">
+                {children}
+              </div>
+            </main>
+					</div>
+					<TanStackDevtools
+						config={{
+							position: "bottom-right",
+						}}
+						plugins={[
+							{
+								name: "Tanstack Router",
+								render: <TanStackRouterDevtoolsPanel />,
+							},
+							TanStackQueryDevtools,
+						]}
+					/>
+				</ClerkProvider>
+				<Scripts />
+			</body>
+		</html>
+	);
+}
